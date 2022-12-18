089a25695d498452e5b402ccda3c2663ea4d41eb
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2022-12-18 17:33:14 +0530

    Update Day1-Basics of operators.cpp

--- Day1-Basics of operators.cpp
+++ Day1-Basics of operators.cpp
@@ -2,7 +2,7 @@
 int main(){
 	int a=5,b=10,c=5;
 	printf("%d\n",-7%2);
-	printf("%d\n",-7%-2);
+	printf("%d\n",-7%-2);      //when it is modulus then sign of output depends on sign of numerator value
 	printf("%d\n",7%2);
 	printf("%d\n",7%-2);
 	printf("%d\n",-7/2);
